ee15d1e9c802ff9097b8051a29b058be92556738
100651838 <aryan.kukreja@ontariotechu.net>
2019-11-27 22:53:40 -0500

    Added Comments to relevant code files.

--- src/SimilarityChecker/Jaccard.java
+++ src/SimilarityChecker/Jaccard.java
@@ -4,42 +4,71 @@
  * Course     - Design and Analysis of Algorithms | SOFE 3770
  *
  * Purpose:
- *      - Assignment # | Assignment Description
+ *      - Major Project | Plagiarism-Detection Algorithm
  *
  * About:
  *      -
  *
- * Input      - {List of Inputs}
- * Output     - {List of Outputs}
+ * Input      - Original string and plagiarized file
+ * Output     - Number of words plagiarized
  *
- * Extra Info - {}
+ * Extra Info - No main method: This program is non-executable
  */
+
+// This is a part of the SimilarityChecker package
 package SimilarityChecker;
 
+// Import all the require packages
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Jaccard Class
+ * - A container class for the core plagiarism
+ */
 public class Jaccard {
-    public static double jaccardImplementation(String wiki, String cheat) {
-        List<String> cheatB = Arrays.asList(cheat.toLowerCase().replaceAll("[^a-z0-9 ]", "").split(" "));
-        String[] senList = wiki.toLowerCase().replaceAll("[^a-z0-9. ]", "").replaceAll("\\. ", ".").split("\\.");
+    /**
+     * jaccardImplementation() static method:
+     * - Holds the implementation of the Jaccard algorithm for comparing strings.
+     *
+     * @param original    The original string passed in
+     * @param plagiarized The plagiarized sentence
+     * @return Return the number of words of copied that are plagiarized
+     */
+    public static double jaccardImplementation(String original, String plagiarized) {
+        // Clean up the strings passed in as variables, and save them as a list
+        // Split the plagiarized string into a bag of words
+        // Split the original string into a bag of sentences.
+        List<String> plagiarizedBag = Arrays.asList(plagiarized.toLowerCase().replaceAll("[^a-z0-9 ]", "").split(" "));
+        String[] originalSentences = original.toLowerCase().replaceAll("[^a-z0-9. ]", "").replaceAll("\\. ", ".").split("\\.");
 
+        // Declare tracker variables
+        // Declare a list to store the copied and original sentence
         double max = 0, maximum = 0;
+        List<String> copied, originalString;
 
-        List<String> copied, sentB;
-        for (String s : senList) {
-            sentB = Arrays.asList(s.split(" "));
-            copied = new ArrayList<>(cheatB);
+        // For every sentence in te original sentences bag
+        for (String sentence : originalSentences) {
+            // Split that sentence into a bag of words
+            // Make a copy of plagiarizedBag
+            originalString = Arrays.asList(sentence.split(" "));
+            copied = new ArrayList<>(plagiarizedBag);
 
-            copied.retainAll(sentB);
+            // Find the intersection of the original and the copied
+            copied.retainAll(originalString);
 
-            double amountPlagiarized = (double) copied.size() / (sentB.size() + cheatB.size() - copied.size());
+            // Calculate the amount of plagiarism between the current original string and the plagiarized one
+            double amountPlagiarized = (double) copied.size() / (originalString.size() + plagiarizedBag.size() - copied.size());
+
+            // Save it if it is the max (plagiarism value and number of words in it)
             if (amountPlagiarized > max) {
                 max = amountPlagiarized;
                 maximum = copied.size();
             }
         }
+
+        // Return the maximum number of words plagiarized
         return maximum;
     }
 }
--- src/WholeFileChecker.java
+++ src/WholeFileChecker.java
@@ -4,29 +4,52 @@
  * Course     - Design and Analysis of Algorithms | SOFE 3770
  *
  * Purpose:
- *      - Assignment # | Assignment Description
+ *      - Major Project | Plagiarism-Detection Algorithm
  *
  * About:
- *      -
+ *      - This program handles 2 entire files: an original file, and a plagiarized (possibly) one. It gets the plagiarism
+ *        amount for the entire file.
  *
- * Input      - {List of Inputs}
- * Output     - {List of Outputs}
+ * Input      - Location of original and plagiarized file
+ * Output     - Plagiarism amount
  *
- * Extra Info - {}
+ * Extra Info - No main method: this file is non-executable
  */
 
+// Import the Jaccard similarity checker method
 import SimilarityChecker.Jaccard;
 
+/**
+ * WholeFileChecker Class
+ * - Used to check similarities between whole files
+ *
+ * @version 1.0.0
+ */
 class WholeFileChecker {
+    /**
+     * checkWholeFile() method:
+     * - Used to check plagiarism between 2 entire files.
+     *
+     * @param origin The original file
+     * @param copied The file to check against the original
+     * @return The plagiarism value
+     */
     static double checkWholeFile(String origin, String copied) {
+        // Split the string
         String[] copiedArray = copied.split("\\. ");
 
+        // Declare a variable to track the total number of variables.
+        // Get the number of words in the copied string.
         int totalPlagiarized = 0;
         int totalWords = copied.split("\\s+").length;
 
+        // For every string in the list of copied sentences
         for (String s : copiedArray) {
+            // Sum up its plagiarized count.
             totalPlagiarized += Jaccard.jaccardImplementation(origin, s);
         }
+
+        // Return their percentage of plagiarism.
         return (double) totalPlagiarized / totalWords;
     }
 }
