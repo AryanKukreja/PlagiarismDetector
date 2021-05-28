900b55096b428a58e46636ace421eaf8201995a0
root <root@DESKTOP-8N671O0.localdomain>
2021-05-27 23:15:24 -0400

    Commenting and documentation changes

--- backend/src/main/java/com/abusyprogrammer/backend/about/AboutService.java
+++ backend/src/main/java/com/abusyprogrammer/backend/about/AboutService.java
@@ -3,7 +3,14 @@ package com.abusyprogrammer.backend.about;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * The About Service implements a REST interface for returning miscellaneous 
+ * information about the backend server to the user
+ * 
+ * @author  Aryan Kukreja
+ * @version 1.0
+ * @since   2021-03-27
+ */
 @RestController
 @RequestMapping(path = "api/misc/")
 public class AboutService {
