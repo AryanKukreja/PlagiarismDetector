150aecc0da06589c0cc5793d8043755a2ed241a8
root <root@DESKTOP-8N671O0.localdomain>
2021-06-03 23:13:08 -0400

    Final updates to complete the first algorithm

--- backend/src/main/java/com/abusyprogrammer/backend/misc/AboutService.java
+++ backend/src/main/java/com/abusyprogrammer/backend/misc/AboutService.java
@@ -1,27 +1,38 @@
 package com.abusyprogrammer.backend.misc;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * The About Service implements a REST interface for returning miscellaneous 
+ * The About Service implements a REST interface for returning miscellaneous
  * information about the backend server to the user
  * 
- * @author  Aryan Kukreja
+ * @author Aryan Kukreja
  * @version 1.0
- * @since   2021-03-27
+ * @since 2021-03-27
  */
 @RestController
-@RequestMapping(path = "api/about")
 public class AboutService {
-    
-    public String about() {
-        return 
-        "{" + 
-            "\"name\": \"Plagiarism Detector Web Application\"," + 
-            "\"about\": \"A RESTful web-application that provides different string-matching algorithms as endpoints for users to experiment with.\"," + 
-            "\"tool\": \"Spring Boot\"" + 
-        "}";
+
+    final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(path = "api/misc/about/")
+    public String about() throws IOException, FileNotFoundException, ParseException {
+        JSONParser jsonParser = new JSONParser();         
+        FileReader reader = new FileReader("src/main/resources/system.properties.json");
+        Object obj = jsonParser.parse(reader);
+
+        JSONObject employeeList = (JSONObject) obj;
+        return employeeList.toString();
     }
 
 }
